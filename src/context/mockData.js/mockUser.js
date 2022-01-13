29ecafac9e3160d3aea231c0bfbc4c81550f3148
systemRockstar <18580997+systemRockstar@users.noreply.github.com>
2022-01-13 15:14:38 +0530

    updated mock data

--- src/context/mockData.js/mockUser.js
+++ src/context/mockData.js/mockUser.js
@@ -2,7 +2,7 @@ export default {
   login: 'john-smilga',
   id: 42133389,
   node_id: 'MDQ6VXNlcjQyMTMzMzg5',
-  avatar_url: 'https://avatars3.githubusercontent.com/u/42133389?v=4',
+  avatar_url: 'https://avatars.githubusercontent.com/u/42133389?v=4',
   gravatar_id: '',
   url: 'https://api.github.com/users/john-smilga',
   html_url: 'https://github.com/john-smilga',
@@ -28,10 +28,10 @@ export default {
   hireable: null,
   bio: 'Creator of Coding Addict',
   twitter_username: 'john_smilga',
-  public_repos: 152,
+  public_repos: 220,
   public_gists: 0,
-  followers: 1495,
+  followers: 8713,
   following: 0,
   created_at: '2018-08-06T06:48:23Z',
-  updated_at: '2020-07-08T05:01:32Z',
-};
+  updated_at: '2021-08-09T02:36:51Z',
+}
